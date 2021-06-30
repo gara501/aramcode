9e6db3b89c64593cd0338f097225a4b575c8bcb1
andres <gandres.ramirez@gmail.com>
2021-06-29 22:05:59 -0500

    Close button menu

--- assets/portfolio.js
+++ assets/portfolio.js
@@ -9,9 +9,18 @@ const menuMobileButton = document.querySelector('.menu-mobile-action');
 const menuMobile = document.querySelector('.menu-mobile');
 const menuMobileItems = document.querySelectorAll('.menu-mobile a');
 const arrow = document.querySelectorAll('.arrow');
+const close = document.querySelector('.close');
 
 menuMobileButton.addEventListener('click', (e)=> {
 	menuMobile.classList.add('show');
+})
+
+menuMobileButton.addEventListener('touchmove', (e)=> {
+	menuMobile.classList.add('show');
+})
+
+close.addEventListener('click', (e) => {
+	menuMobile.classList.remove('show');
 })
 
 menuMobileItems.forEach(item => {
